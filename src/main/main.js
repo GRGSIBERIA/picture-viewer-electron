3de30324f0c2982604819ca950eda1a568944967
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-08-18 17:17:44 +0900

    move renderer files from static to renderer

--- src/main/main.js
+++ src/main/main.js
@@ -37,16 +37,11 @@ function createMainWindow() {
     }
   
     if (isDevelopment) {
-        //window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`);
-        window.loadURL(formatUrl({
-            pathname: path.join(__static, '/index.html'),
-            protocol: 'file',
-            slashes: true
-        }));
+        window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`);
     }
     else {
         window.loadURL(formatUrl({
-            pathname: path.join(__dirname, '/index.html'),
+            pathname: path.join(__dirname, 'index.html'),
             protocol: 'file',
             slashes: true
         }));
