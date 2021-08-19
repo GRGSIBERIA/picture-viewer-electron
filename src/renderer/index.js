3478e95922541e1ebbd8afee2fee2f4832d3c4b4
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-08-19 21:33:02 +0900

    modify typo

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -2,7 +2,7 @@
 
 document.getElementById('import-button').addEventListener('change', event => {
     for (let i = 0; i < event.target.files.length; ++i) {
-        let file = ev.target.files[i];
+        let file = event.target.files[i];
 
         let relativePath = file.webkitRelativePath;
 
