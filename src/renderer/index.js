10b9782d4ee8554b0a43ab07c318deac0cc044fb
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-08-19 21:29:13 +0900

    add import event

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,4 +1,11 @@
-/**
-document.querySelector("#import-button").addEventListener("click", async () => {
-    
-});*/
+
+
+document.getElementById('import-button').addEventListener('change', event => {
+    for (let i = 0; i < event.target.files.length; ++i) {
+        let file = ev.target.files[i];
+
+        let relativePath = file.webkitRelativePath;
+
+        console.log(relativePath);
+    }
+});
