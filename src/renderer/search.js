f23743df5dc0def3d1cf9f9f345d8d2dc6e7a533
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-08-25 22:16:10 +0900

    add search options

--- src/renderer/search.js
+++ src/renderer/search.js
@@ -16,6 +16,33 @@ window.myapi.on("showSearchThumbnails", (event, docs) => {
     }
 });
 
-document.getElementById("tab-search").addEventListener("click", async (event) => {
+window.onload = async (event) => {
     await window.myapi.find(emptyQuery);
+}
+
+function getNumofListings() {
+    const elem = document.getElementById("numof-listings");
+    return Number(elem.value);
+}
+
+function getOrder() {
+    const elem = document.getElementById("listing-order");
+    return Number(elem.value);
+}
+
+function getOrderPriority() {
+    const elem = document.getElementById("order-priority");
+    return elem.value;
+}
+
+document.getElementById("search-keyword").addEventListener('keypress', async (event) => {
+    if (event.key === 'Enter') {
+        const limit = getNumofListings();
+        const order = getOrder();
+        const priority = getOrderPriority();
+        const param = {
+            limit: limit,
+            
+        }
+    }
 });
